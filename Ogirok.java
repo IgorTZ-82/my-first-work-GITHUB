cfacb6e71c6fc8cc7c34b314474d0ef32f5867e6
IgorTZ-82 <79048975+IgorTZ-82@users.noreply.github.com>
2021-02-15 14:52:50 +0200

    Add files via upload

---
+++ Ogirok.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+public class Ogirok {
+void bullbeArrayAscending (int [] array){
+    boolean swapped = false;
+    int size = array.length;
+    do {
+        for (int i=0; i< size-1; i++){
+            for(int j=0; j<size-1 -i; j++){
+                if(array[j]> array[j+1]){
+                    int temp =array[j];
+                    array[j]= array[j+1];
+                    array[j+1]=temp;
+                    swapped=true;
+        }
+        }
+        }
+        }while(swapped=false);
+        }
+
+    public static void main(String[] args) {
+        int [] igorok ={5,4,3,2,1};
+        Ogirok sort= new Ogirok();
+        sort.bullbeArrayAscending(igorok);
+        System.out.println(Arrays.toString(igorok));
+    }
+
+
+    }
+
